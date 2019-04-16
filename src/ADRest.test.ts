f3cf5a74e43c7f53158f8853c1d44e61eef4a29c
Ahmad Dehnavi <ahmad.829@gmail.com>
2019-04-17 01:31:45 +0430

    add first test

--- src/ADRest.test.ts
+++ src/ADRest.test.ts
@@ -3,15 +3,16 @@ import ADResponse from "./ADResponse";
 import ADRequest from "./ADRequest";
 
 describe('ADRest Test', () => {
-    it('should ', async () => {
-        let body = JSON.stringify({
-            'test': 'test-value'
-        });
+    let body = JSON.stringify({
+        'test': 'test-value'
+    });
+    let res: ADResponse<any>;
+    beforeEach(async () => {
         let rest = new ADRest({
             async fetch<T>(request: Readonly<ADRequest>): Promise<ADResponse<T>> {
                 return new ADResponse({
                     body: body,
-                    headers: {},
+                    headers: {'test': 'test header'},
                     status: 200,
                     statusText: 'text'
                 })
@@ -19,8 +20,25 @@ describe('ADRest Test', () => {
         });
 
         let req = new ADRequest.Builder().url('url').build();
-        let res = await rest.process(req);
+        res = await rest.process(req);
+    });
+
+    it('should response.text() work correctly', async () => {
         expect(await res.text()).toBe(body);
+    });
+
+    it('should response.json() work correctly', async () => {
         expect((await res.json())['test']).toBe(JSON.parse(body)['test'])
+    });
+
+    it('should response.status work correctly', async () => {
+        expect(await res.status).toBe(200)
+    });
+    it('should response.headers work correctly', async () => {
+        expect(await res.headers['test']).toBe('test header')
+    });
+
+    it('should response.statusText work correctly', async () => {
+        expect(await res.statusText).toBe('text')
     });
 });
